b49ba87d70b1bd7f9758f71dc233d996908fe635
Gracye.dev777 <248383100+gracyedev777@users.noreply.github.com>
2025-12-12 09:47:53 -0300

    uma lib brasileiro de c++

---
+++ brasil.lib.h.cpp
@@ -0,0 +1,37 @@
+#ifndef BRASIL_LIB_H
+#define BRASIL_LIB_H
+
+#include <iostream>
+#include <string>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+#define inicio int main(){
+#define fim }
+#define retorna return
+#define se if
+#define senao else
+#define enquanto while
+#define para for
+#define escolha switch
+#define caso case
+#define padrao default
+#define pare break
+#define continuea continue
+#define escreva cout <<
+#define leia cin >>
+#define fiml << endl
+#define limpar_tela system("clear")
+#define verdadeiro true
+#define falso false
+#define e &&
+#define ou ||
+#define nao !
+#define aleatorio rand()
+#define iniciar_aleatorio srand(time(NULL))
+
+#endif
+
+
+
